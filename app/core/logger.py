b3634a101547312dede230a7af9a52b3e1ba644b
Sebastian Nagel <sebastian.nagel1@gmx.de>
2025-08-27 22:34:27 +0200

    renamed logger

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,7 +1,7 @@
 import logging
 
-logger = logging.getLogger("chat_stream")
-logger.setLevel(logging.INFO)
+logger = logging.getLogger("chatbot")
+logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
 handler.setFormatter(formatter)
