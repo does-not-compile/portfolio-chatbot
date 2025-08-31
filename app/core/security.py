f2555ce8c21223c49fc3259eada5050e212cd91b
Sebastian Nagel <sebastian.nagel1@gmx.de>
2025-08-31 16:44:35 +0200

    added checks if user actually exists

--- app/core/security.py
+++ app/core/security.py
@@ -1,27 +1,39 @@
+from core.config import settings
+from core.logger import logger
+from datetime import datetime, timedelta, timezone
+from db.session import get_db
+from db import crud
 from fastapi import Request
 import jwt
-from datetime import datetime, timedelta, timezone
-from core.config import settings
+from sqlalchemy.orm import Session
+from typing import Optional
 
 
-def create_jwt(user_id: str):
+def create_jwt(user_id: str) -> str:
     payload = {"sub": user_id, "exp": datetime.now(timezone.utc) + timedelta(days=7)}
     return jwt.encode(payload, settings.JWT_SECRET, algorithm="HS256")
 
 
-def verify_jwt(token: str):
+def verify_jwt(token: str) -> Optional[str]:
+    payload = None
     try:
         payload = jwt.decode(token, settings.JWT_SECRET, algorithms=["HS256"])
         return payload["sub"]
+    except jwt.InvalidSignatureError:
+        logger.error(f"signature of token is invalid.")
     except jwt.ExpiredSignatureError:
-        raise ValueError("Token expired")
+        logger.error(f"token for {payload['sub']} is expired.")
     except jwt.InvalidTokenError:
-        raise ValueError("Invalid token")
+        logger.error(f"token for {payload['sub']} is invalid.")
+    return None
 
 
-def get_current_user(request: Request):
+def get_current_user(request: Request, db: Session) -> Optional[str]:
     token = request.cookies.get("access_token")
-    if not token:
-        return False
-    user_id = verify_jwt(token)
-    return user_id
+    user_id = None
+    if token:
+        user_id = verify_jwt(token)
+    if user_id:
+        if crud.get_user(db, user_id):
+            return user_id
+    return None
